6a01275e2ad7cbb6e1804e946e8650dee46b721b
i20sss20-maker <i20sss20@gmail.com>
2026-08-30 13:30:35 +0300

    feat: add QR encoder for BLOFY activation screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.google.zxing:core:3.5.3")
 
     testImplementation("junit:junit:4.13.2")
 }
