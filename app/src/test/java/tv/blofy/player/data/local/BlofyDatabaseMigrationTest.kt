935ddc4d3ceb667a9e27611e9f89130fdf2ec812
i20sss20-maker <i20sss20@gmail.com>
2026-09-03 12:53:50 +0300

    test(rc07): cover database migration 8 to 9

--- app/src/test/java/tv/blofy/player/data/local/BlofyDatabaseMigrationTest.kt
+++ app/src/test/java/tv/blofy/player/data/local/BlofyDatabaseMigrationTest.kt
@@ -11,7 +11,16 @@ class BlofyDatabaseMigrationTest {
         }
 
         assertEquals(
-            listOf(1 to 2, 2 to 3, 3 to 4, 4 to 5, 5 to 6, 6 to 7, 7 to 8),
+            listOf(
+                1 to 2,
+                2 to 3,
+                3 to 4,
+                4 to 5,
+                5 to 6,
+                6 to 7,
+                7 to 8,
+                8 to 9
+            ),
             migrationPath
         )
         assertEquals(BLOFY_DATABASE_VERSION, migrationPath.last().second)
