44569684741ba2fdc91a1ac2f52e274d9467ba9e
i20sss20-maker <i20sss20@gmail.com>
2026-09-06 19:00:24 +0300

    feat(cloud): register profile cloud API

--- services/activation/src/bootstrap.mjs
+++ services/activation/src/bootstrap.mjs
@@ -2,4 +2,5 @@ import './subscriber-proxy-hook.mjs';
 import './subscriber-portal-ui-hook.mjs';
 import './subscription-hook.mjs';
 import './payment-checkout-hook.mjs';
+import './profile-cloud-hook.mjs';
 await import('./server.mjs');
