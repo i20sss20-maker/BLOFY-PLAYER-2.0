ba12faf189f68a7f3cbf90eeae58b51de2ee35f8
i20sss20-maker <i20sss20@gmail.com>
2026-09-07 00:01:05 +0300

    chore(release): bump RC07 signed build to rc07.3 for in-place upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "tv.blofy.player.v2"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2000010
-        versionName = "2.0.0-rc07.2"
+        versionCode = 2000011
+        versionName = "2.0.0-rc07.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "ACTIVATION_BASE_URL", "\"$activationBaseUrlEscaped\"")
         buildConfigField("String", "BUILD_SHA", "\"$buildShaEscaped\"")
