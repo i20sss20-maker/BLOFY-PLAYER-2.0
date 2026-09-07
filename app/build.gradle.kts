849a0123f4a12b0fa09b3b1f48ad32c04e0eae50
i20sss20-maker <i20sss20@gmail.com>
2026-09-08 00:02:59 +0300

    rc07.12: bump signed release version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "tv.blofy.player.v2"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2000019
-        versionName = "2.0.0-rc07.11"
+        versionCode = 2000020
+        versionName = "2.0.0-rc07.12"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "ACTIVATION_BASE_URL", "\"$activationBaseUrlEscaped\"")
         buildConfigField("String", "BUILD_SHA", "\"$buildShaEscaped\"")
