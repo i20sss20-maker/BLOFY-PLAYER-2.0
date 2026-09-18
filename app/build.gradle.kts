78a2d6f1a136f64374d36ceaf9c3b1464e6f6c48
saleh musaed alharbi <i20sss20@gmail.com>
2026-09-18 11:20:35 +0300

    Bump test build to rc07.52

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "tv.blofy.player.v2"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2000063
-        versionName = "2.0.0-rc07.51"
+        versionCode = 2000064
+        versionName = "2.0.0-rc07.52"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "ACTIVATION_BASE_URL", "\"$activationBaseUrlEscaped\"")
         buildConfigField("String", "UPDATE_BASE_URL", "\"$updateBaseUrlEscaped\"")
