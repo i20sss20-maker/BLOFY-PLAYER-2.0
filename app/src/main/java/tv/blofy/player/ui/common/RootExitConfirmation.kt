8c5f5179e801941952520e96e59733aaed7426ae
saleh musaed alharbi <i20sss20@gmail.com>
2026-09-22 16:30:52 +0300

    Keep theme polish compatible with older Android TVs

--- app/src/main/java/tv/blofy/player/ui/common/RootExitConfirmation.kt
+++ app/src/main/java/tv/blofy/player/ui/common/RootExitConfirmation.kt
@@ -7,7 +7,6 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
-import android.graphics.drawable.GradientDrawable
 import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.LinearLayout
